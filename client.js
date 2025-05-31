85482f94a5e43d6e726688690affb467e5683f4d
Qwertycal <souldata2000@yahoo.com>
2025-05-31 20:22:21 +0300

    added client ability read local event file and send it to server side, server side can get data and store in local file, added authenticate for secure requests

---
+++ client.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const readline = require('readline');
+const axios = require('axios');
+
+require('dotenv').config();
+
+const SERVER_URL = process.env.SERVER_URL;
+const AUTH_SECRET = process.env.AUTH_SECRET;
+const EVENTS_FILE = process.env.STREAM_FILE;
+console.log('EVENTS_FILE: ', EVENTS_FILE);
+
+
+async function readFileAndSendEvents() {
+    try {
+        // Check if exists file with events 
+        if (!fs.existsSync(EVENTS_FILE)) {
+            console.error(`Error: ${EVENTS_FILE} file not found`);
+            return;
+        }
+
+        const fileStream = fs.createReadStream(EVENTS_FILE);
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        for await (const newLine of rl) {
+            try {
+                const event = JSON.parse(newLine);
+                const response = await axios.post(SERVER_URL, event, {
+                    headers: {
+                        'Authorization': AUTH_SECRET,
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                if (response.status === 201) {
+                    console.log(`Event was sent successfully:`, event);
+                }
+            }
+            catch (error) {
+                console.error(`Error parsing JSON: ${error.message}`);
+                continue;
+            }
+        }
+
+    } catch (error) {
+        console.error(`Error on checking if file exsists: ${error.message}`);
+        return;
+    }
+}
+
+readFileAndSendEvents();
--- server.js
+++ server.js
@@ -1,25 +1,41 @@
-require('dotenv').config();
-const express = require('express');
 
+const express = require('express');
+const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+const authenticate = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const secret = process.env.AUTH_SECRET;
+    if (authHeader !== secret) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+    next();
+};
 
-app.post('/liveEvent', (req, res) => {
-    console.log('req:', req.body);
-    
-     try {
-    } 
-    catch (error) {
+app.post('/liveEvent', [authenticate], (req, res) => {
+    try {
+        const event = req.body;
+        const eventsFile = process.env.SERVER_EVENTS;
+        const eventLine = JSON.stringify(event) + '\n';
+        fs.appendFileSync(eventsFile, eventLine);
+        return res.status(201).json({ message: 'Event saved successfully' });
+
+    } catch (error) {
+        console.error('Error processing event:', error);
+        res.status(500).json({ error: 'Internal server error', details: error.message });
     }
 });
 
 app.get('/userEvents/:userid', async (req, res) => {
     console.log('req:', req.params);
     try {
-    } 
+    }
     catch (error) {
     }
 });
